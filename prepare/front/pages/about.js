fd7c87fa5ca23b8ea7994ff57268dceb0dc30cfc
heedo-wang <dhkdgmleh@gmail.com>
2021-10-28 18:33:18 +0900

    change about.js

--- prepare/front/pages/about.js
+++ prepare/front/pages/about.js
@@ -44,7 +44,15 @@ const About = () => {
     );
 };
 
-export const getStaticProps = wrapper.getStaticProps(async (context) => {
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+    const cookie = context.req ? context.req.headers.cookie : '';
+    axios.defaults.headers.Cookie = '';
+    if (context.req && cookie) {
+        axios.defaults.headers.Cookie = cookie;
+    }
+     context.store.dispatch({
+        type: LOAD_MY_INFO_REQUEST,
+    });
     context.store.dispatch({
         type: LOAD_USER_REQUEST,
         data: 2,
